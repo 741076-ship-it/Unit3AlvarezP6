9f80230d191b7dd06414dd19d5619bb60764d92b
741076-ship-it <741076@student.dusd.net>
2026-04-02 13:46:56 -0700

    finished stop move left after game over

--- P6AlvarezUnit3/Assets/Scripts/PlayerController.cs
+++ P6AlvarezUnit3/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     public float jumpforce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,8 +25,18 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void OnCollistionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+       
+
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
     }
 }
